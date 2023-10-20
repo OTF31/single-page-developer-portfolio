5036ce65c0021062ac62ff0e15e3ef64bcbc7095
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-10-20 18:25:30 -0500

    add button component
    - add assets
    - set bg main bg color
    - change app title and icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import { Title } from '@mantine/core'
+import { AppShell, AppShellMain, Title } from '@mantine/core'
+
+import classes from './page.module.css'
 
 const Home = () => {
-  return <Title>Single-page developer portfolio</Title>
+  return (
+    <AppShell>
+      <AppShellMain className={classes.main}>
+        <Title c='white'>Single-page developer portfolio</Title>
+      </AppShellMain>
+    </AppShell>
+  )
 }
 
 export default Home
