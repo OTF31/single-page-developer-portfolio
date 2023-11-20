5658e762663e201f00ff801ee3cb60e00d7305ac
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-11-20 11:15:31 -0500

    add tablet design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import {
   AppShellMain,
   BackgroundImage,
   Box,
+  Flex,
   Group,
   Image,
   Stack,
   Text,
   Title,
+  SimpleGrid,
 } from '@mantine/core'
 import NextImage from 'next/image'
 
@@ -16,6 +18,7 @@ import iconGithub from '@/assets/images/icon-github.svg'
 import iconLinkedin from '@/assets/images/icon-linkedin.svg'
 import iconTwitter from '@/assets/images/icon-twitter.svg'
 import imageProfileMobile from '@/assets/images/image-profile-mobile.webp'
+import imageProfileDesktop from '@/assets/images/image-profile-desktop.webp'
 import patternCircle from '@/assets/images/pattern-circle.svg'
 import patternRings from '@/assets/images/pattern-rings.svg'
 import thumbnailProject1Large from '@/assets/images/thumbnail-project-1-large.webp'
@@ -87,80 +90,114 @@ const Home = () => {
   return (
     <AppShell>
       <AppShellMain className={classes.main}>
-        <Box className={classes.author__container}>
-          <BackgroundImage
-            src={imageProfileMobile.src}
-            className={classes.author__background}
-          >
-            <Stack className={classes.author__content}>
-              <Text className={classes.author__name}>adamkeyes</Text>
-              <Group className={classes['author__social-container']}>
-                <Image component={NextImage} src={iconGithub} width={20} alt='github' />
-                <Image
-                  component={NextImage}
-                  src={iconFrontendMentor}
-                  width={20}
-                  alt='frontend mentor'
-                />
-                <Image
-                  component={NextImage}
-                  src={iconLinkedin}
-                  width={20}
-                  alt='linkedin'
-                />
-                <Image component={NextImage} src={iconTwitter} width={20} alt='twitter' />
-              </Group>
-            </Stack>
-          </BackgroundImage>
-          <Image
-            component={NextImage}
-            src={patternRings}
-            w={530}
-            pos='absolute'
-            top={124}
-            right={190}
-            alt='pattern-rings'
-          />
-          <Image
-            component={NextImage}
-            src={patternCircle}
-            w={129}
-            pos='absolute'
-            top={254}
-            right={-60}
-            alt='pattern-circle'
-          />
-        </Box>
-
-        <Stack className={classes['introduction__container']}>
-          <Text className={classes.introduction__title}>
-            Nice to meet you! I&apos;m{' '}
-            <Text
-              component='span'
-              inherit
-              className={classes['introduction__title--highlight']}
+        <Flex direction={{ base: 'column', sm: 'row-reverse' }} gap={0}>
+          <Box className={classes.author__container}>
+            <BackgroundImage
+              src={imageProfileMobile.src}
+              className={classes.author__background}
             >
-              Adam Keyes
+              <Stack className={classes.author__content}>
+                <Text className={classes.author__name}>adamkeyes</Text>
+                <Group className={classes['author__social-container']}>
+                  <Image
+                    component={NextImage}
+                    src={iconGithub}
+                    w={{ sm: 24 }}
+                    alt='github'
+                  />
+                  <Image
+                    component={NextImage}
+                    src={iconFrontendMentor}
+                    w={{ sm: 24 }}
+                    alt='frontend mentor'
+                  />
+                  <Image
+                    component={NextImage}
+                    src={iconLinkedin}
+                    w={{ sm: 24 }}
+                    alt='linkedin'
+                  />
+                  <Image
+                    component={NextImage}
+                    src={iconTwitter}
+                    w={{ sm: 24 }}
+                    alt='twitter'
+                  />
+                </Group>
+              </Stack>
+              <Image
+                component={NextImage}
+                src={patternCircle}
+                w={129}
+                pos='absolute'
+                visibleFrom='sm'
+                bottom={0}
+                right={-60}
+                alt='pattern-circle'
+              />
+            </BackgroundImage>
+            <Image
+              component={NextImage}
+              src={patternRings}
+              w={530}
+              pos='absolute'
+              top={140}
+              left={-350}
+              hiddenFrom='sm'
+              alt='pattern-rings'
+            />
+            <Image
+              component={NextImage}
+              src={patternCircle}
+              w={129}
+              pos='absolute'
+              right={-60}
+              hiddenFrom='sm'
+              top={254}
+              bottom={-50}
+              alt='pattern-circle'
+            />
+          </Box>
+
+          <Stack className={classes['introduction__container']}>
+            <Text className={classes['author__name--tablet']}>adamkeyes</Text>
+            <Text className={classes.introduction__title}>
+              Nice to meet you! I&apos;m{' '}
+              <Text component='span' inherit>
+                Adam Keyes
+              </Text>
+              .
             </Text>
-            .
-          </Text>
-          <Text className={classes.introduction__text}>
-            Based in the UK, I&apos;m a front-end developer passionate about building
-            accessible web apps that users love.
-          </Text>
-          <Button>CONTACT ME</Button>
-        </Stack>
+            <Text mt={{ sm: 60 }}>
+              Based in the UK, I&apos;m a front-end developer passionate about building
+              accessible web apps that users love.
+            </Text>
+            <Button>CONTACT ME</Button>
+            <Image
+              component={NextImage}
+              src={patternRings}
+              w={530}
+              pos='absolute'
+              top={20}
+              left={-280}
+              visibleFrom='sm'
+              alt='pattern-rings'
+            />
+          </Stack>
+        </Flex>
+
         <Box className={classes.skills__container}>
-          <Stack className={classes['skills__content-container']}>
+          <SimpleGrid
+            className={classes['skills__content-container']}
+            cols={{ base: 1, sm: 2 }}
+          >
             {SKILLS.map((skill, index) => (
               <Stack key={index} className={classes['skills__item-container']}>
                 <Text className={classes['skills__item-skill']}>{skill.name}</Text>
-                <Text className={classes['skills__item-years']}>
-                  {skill.yearsExperience} Years Experience
-                </Text>
+                <Text c='orochimaru'>{skill.yearsExperience} Years Experience</Text>
               </Stack>
             ))}
-          </Stack>
+          </SimpleGrid>
           <Image
             component={NextImage}
             src={patternRings}
@@ -168,6 +205,17 @@ const Home = () => {
             pos='absolute'
             bottom={-60}
             left={180}
+            hiddenFrom='sm'
+            alt='pattern-rings'
+          />
+          <Image
+            component={NextImage}
+            src={patternRings}
+            w={530}
+            pos='absolute'
+            bottom={-10}
+            right={-350}
+            visibleFrom='sm'
             alt='pattern-rings'
           />
         </Box>
@@ -177,66 +225,97 @@ const Home = () => {
             <Title className={classes['projects__header-title']}>Projects</Title>
             <Button>CONTACT ME</Button>
           </Group>
-          {PROJECTS.map((project, index) => (
-            <Stack key={index} className={classes['projects__item-container']}>
-              <Image src={project.thumbailSmall} component={NextImage} alt='thumbnail' />
-              <Stack className={classes['projects__item-text-container']}>
-                <Text className={classes['projects__item-title']}>{project.title}</Text>
-                <Group className={classes['projects__item-topics-container']}>
-                  {project.topics.map((topic, index) => (
-                    <Text key={index} className={classes['projects__item-topic']}>
-                      {topic}
-                    </Text>
-                  ))}
+          <SimpleGrid
+            cols={{ base: 1, sm: 2 }}
+            spacing={{ base: 40, sm: 22 }}
+            verticalSpacing={{ base: 40, sm: 60 }}
+          >
+            {PROJECTS.map((project, index) => (
+              <Stack key={index} className={classes['projects__item-container']}>
+                <Image
+                  src={project.thumbailSmall}
+                  component={NextImage}
+                  alt='thumbnail'
+                />
+                <Stack className={classes['projects__item-text-container']}>
+                  <Text className={classes['projects__item-title']}>{project.title}</Text>
+                  <Group className={classes['projects__item-topics-container']}>
+                    {project.topics.map((topic, index) => (
+                      <Text key={index} className={classes['projects__item-topic']}>
+                        {topic}
+                      </Text>
+                    ))}
+                  </Group>
+                </Stack>
+                <Group className={classes['projects__item-actions-container']}>
+                  <Button>VIEW PROJECT</Button>
+                  <Button>VIEW CODE</Button>
                 </Group>
               </Stack>
-              <Group className={classes['projects__item-actions-container']}>
-                <Button>VIEW PROJECT</Button>
-                <Button>VIEW CODE</Button>
-              </Group>
-            </Stack>
-          ))}
+            ))}
+          </SimpleGrid>
         </Stack>
 
         <Stack className={classes['contact__container']}>
           <Stack className={classes['contact__form-container']}>
             <Stack className={classes['contact__form-text-container']}>
               <Text className={classes['contact__form-title']}>Contact</Text>
-              <Text className={classes['contact__form-text']}>
+              <Text c='orochimaru'>
                 I would love to hear about your project and how I could help. Please fill
                 in the form, and I&apos;ll get back to you as soon as possible.
               </Text>
             </Stack>
             <Stack className={classes['contact__form-inputs-container']}>
-              <TextInput placeholder='NAME' />
+              <TextInput placeholder='NAME' error='Sorry, invalid format here' />
               <TextInput placeholder='EMAIL' />
               <TextArea placeholder='MESSAGE' />
               <Button className={classes['contact__form-submit']}>SEND MESSAGE</Button>
             </Stack>
           </Stack>
           <Box className={classes['contact__divider']} />
-          <Stack className={classes['contact__social-container']}>
+          <Flex className={classes['contact__social-container']}>
             <Text className={classes['contact__author-name']}>adamkeyes</Text>
             <Group className={classes['contact__social-icons-container']}>
-              <Image component={NextImage} src={iconGithub} width={20} alt='github' />
+              <Image component={NextImage} src={iconGithub} w={{ sm: 24 }} alt='github' />
               <Image
                 component={NextImage}
                 src={iconFrontendMentor}
-                width={20}
+                w={{ sm: 24 }}
                 alt='frontend mentor'
               />
-              <Image component={NextImage} src={iconLinkedin} width={20} alt='linkedin' />
-              <Image component={NextImage} src={iconTwitter} width={20} alt='twitter' />
+              <Image
+                component={NextImage}
+                src={iconLinkedin}
+                w={{ sm: 24 }}
+                alt='linkedin'
+              />
+              <Image
+                component={NextImage}
+                src={iconTwitter}
+                w={{ sm: 24 }}
+                alt='twitter'
+              />
             </Group>
-          </Stack>
+          </Flex>
           <Image
             src={patternRings}
             component={NextImage}
             alt='pattern-rings'
             pos='absolute'
+            hiddenFrom='sm'
             top={445}
             w={530}
             right={190}
+          />
+          <Image
+            src={patternRings}
+            component={NextImage}
+            alt='pattern-rings'
+            pos='absolute'
+            visibleFrom='sm'
+            w={530}
+            bottom={180}
+            left={-370}
           />
         </Stack>
       </AppShellMain>
