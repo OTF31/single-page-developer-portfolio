9d9f2a723d73c5f0a237a3a510758ca95d51fd64
Denis Omar Cuyo Ttito <75378049+OTF31@users.noreply.github.com>
2023-11-25 16:45:45 -0500

    add desktop design (#4)
    * add desktop design
    
    * update introduction contact button

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,13 +1,14 @@
-import { TextInput as MTextInput, TextInputProps } from '@mantine/core'
+import { Image, TextInput as MTextInput, TextInputProps } from '@mantine/core'
 
+import iconError from '@/assets/images/icon-error.png'
 import classes from './TextInput.module.css'
 
 type Props = TextInputProps
 
 const TextInput = (props: Props) => {
-  const { ...rest } = props
+  const { rightSection = <Image src={iconError.src} />, ...rest } = props
 
-  return <MTextInput classNames={classes} {...rest} />
+  return <MTextInput classNames={classes} {...{ rightSection }} {...rest} />
 }
 
 export default TextInput
